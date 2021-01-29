8c0467db6bf71b488bb477b38473d02ea72369e2
Choharim <jhl8586@naver.com>
2021-01-29 14:57:48 +0900

    Add festivalData of thisMonth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [festivalData, setFestivalData] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
-        `http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival?serviceKey=${API_KEY}&MobileOS=ETC&MobileApp=AppTest&arrange=A&listYN=Y`
-      )
-      .then((response) => console.log(response));
-  });
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const date = new Date();
+    const NOW = `${date.getFullYear()}${
+      date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+    }01`;
 
+    const getFestivalData = async () => {
+      const festival = await axios
+        .get(
+          `http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival?serviceKey=${API_KEY}&MobileOS=ETC&MobileApp=AppTest&arrange=A&listYN=Y&eventStartDate=${NOW}`
+        )
+        .then((res) => {
+          return res.data.response.body.items.item;
+        });
+      setFestivalData(festival);
+    };
+
+    getFestivalData();
+  }, []);
+
+  console.log(festivalData);
   return <div className="App">start</div>;
 }
 
