9075d6bd1b1854a8a897f480233e3f4836ddefbb
Choharim <jhl8586@naver.com>
2021-01-31 14:11:29 +0900

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import HomePage from "pages/Home/index";
 
 const App = () => {
   return (
     <div className="App">
-      <GlobalStyle />
-      <HomePage />
+      <Router>
+        <GlobalStyle />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
@@ -21,5 +28,15 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     box-sizing: border-box;
+  }
+  a {
+    text-decoration:none;
+    color:black;
+    &:hover {
+      color:black;
+    }
+    &:visited {
+      color:black;
+    }
   }
 `;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { BiCalendarHeart } from "react-icons/bi";
-import { FaCloudRain, FaCloud, FaRegSnowflake } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { FaCloudRain, FaCloud, FaRegSnowflake, FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 import { FiSun } from "react-icons/fi";
 import { IoIosThunderstorm } from "react-icons/io";
 
 const Navbar = () => {
   const WEATHER_KEY = process.env.REACT_APP_WEATHER_KEY;
+
+  const [showNav, setShowNav] = useState(false);
   const [weather, setWeather] = useState("");
   const [weatherLoading, setWeatherLoading] = useState(false);
 
@@ -45,17 +48,31 @@ const Navbar = () => {
       } else {
         return <Thunder />;
       }
+    } else {
+      return <Weather>위치를 켜주세요</Weather>;
     }
   };
 
   return (
-    <NavbarContainer>
-      <Logo>어디갈까?</Logo>
-      <div>
-        {weatherIcon()}
-        <FavIcon />
-      </div>
-    </NavbarContainer>
+    <>
+      <NavbarContainer>
+        <Logo>어디갈까?</Logo>
+        <NavBar onClick={() => setShowNav(true)} />
+      </NavbarContainer>
+      <NavBarBox showNav={showNav}>
+        <CloseBtn onClick={() => setShowNav(false)} />
+        <UrlBox>
+          <Link to="/">홈</Link>
+          <Link to="/festivals">어디갈까, 축제</Link>
+          <Link to="/bookMark">가고싶은, 축제</Link>
+          <Link to="/logIn">로그인</Link>
+        </UrlBox>
+        <WeatherBox>
+          {weatherLoading && <Weather>오늘의 날씨</Weather>}
+          {weatherIcon()}
+        </WeatherBox>
+      </NavBarBox>
+    </>
   );
 };
 
@@ -76,26 +93,82 @@ const Logo = styled.span`
   font-size: 23px;
 `;
 
-const Rain = styled(FaCloudRain)`
+const NavBar = styled(FaBars)`
   font-size: 23px;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const NavBarBox = styled.div`
+  position: fixed;
+  top: 0;
+  right: -300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+  height: calc(100% - 40px);
+  padding: 20px;
+  visibility: hidden;
+  opacity: 0;
+  transition: 0.2s ease;
+  ${(props) =>
+    props.showNav &&
+    css`
+      right: 0;
+      visibility: visible;
+      opacity: 1;
+    `}
+  z-index: 100;
+  background-color: #fff;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  font-size: 23px;
+  > div,
+  span {
+    font-family: "Stylish", sans-serif;
+  }
+`;
+
+const CloseBtn = styled(AiOutlineClose)`
+  align-self: flex-end;
+`;
+
+const UrlBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 180px;
+`;
+
+const WeatherBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Weather = styled.span`
+  margin-right: 10px;
+`;
+
+const Rain = styled(FaCloudRain)`
   margin-right: 20px;
   color: #37a7d8;
 `;
 
 const Cloud = styled(FaCloud)`
-  font-size: 23px;
   margin-right: 20px;
   color: #3656b2;
 `;
 
 const Snow = styled(FaRegSnowflake)`
-  font-size: 23px;
   margin-right: 20px;
   color: #e1e7f6;
 `;
 
 const Thunder = styled(IoIosThunderstorm)`
-  font-size: 23px;
   margin-right: 20px;
   color: #78787a;
 `;
@@ -104,8 +177,4 @@ const Sun = styled(FiSun)`
   font-size: 23px;
   margin-right: 20px;
   color: #f63838;
-`;
-
-const FavIcon = styled(BiCalendarHeart)`
-  font-size: 23px;
 `;
