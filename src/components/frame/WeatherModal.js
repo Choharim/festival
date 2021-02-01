f6c00bd1d0dfcfef85f099a11751cbb4872381e7
Choharim <jhl8586@naver.com>
2021-02-01 10:49:20 +0900

    Add modalContents

--- src/components/frame/WeatherModal.js
+++ src/components/frame/WeatherModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { AiOutlineClose } from "react-icons/ai";
 import clear from "images/clear.png";
 import clouds from "images/clouds.png";
 import rain from "images/rain.png";
@@ -24,17 +25,19 @@ const WeatherModal = ({ showWeather, setShowWeather, weather }) => {
   return (
     <Bg showWeather={showWeather} onClick={() => setShowWeather(false)}>
       <ModalContainer onClick={(e) => e.stopPropagation()}>
+        <CloseBtn onClick={() => setShowWeather(false)} />
         <Title>오늘의 날씨</Title>
         {Object.keys(weather).length !== 0 ? (
           <>
             <Img src={weatherIcon(weather.weather[0].main)} />
+            <Text>{weather.name}</Text>
             <TextContainer>
               <TextBox>
                 <Text>현재 온도 : {weather.main.temp} °C</Text>
-                <Text>현재 습도 : {weather.main.humidity} </Text>
+                <Text>최고 온도 : {weather.main.temp_max} °C</Text>
               </TextBox>
               <TextBox>
-                <Text>최고 온도 : {weather.main.temp_max} °C</Text>
+                <Text>풍속 : {weather.wind.speed} m/s</Text>
                 <Text>최저 온도 : {weather.main.temp_min} °C</Text>
               </TextBox>
             </TextContainer>
@@ -90,6 +93,12 @@ const ModalContainer = styled.div`
   }
 `;
 
+const CloseBtn = styled(AiOutlineClose)`
+  align-self: flex-end;
+  font-size: 23px;
+  cursor: pointer;
+`;
+
 const Title = styled.span`
   font-size: 35px;
 `;
@@ -102,16 +111,20 @@ const Img = styled.img`
 
 const Text = styled.span`
   font-size: 23px;
+  :not(:last-child) {
+    margin-bottom: 10px;
+  }
 `;
 
 const TextContainer = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: space-around;
+  width: 100%;
 `;
 
 const TextBox = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  flex-direction: column;
+  align-items: flex-start;
 `;
