00f0cff941318263183ec573a2d8b795320e122d
Choharim <jhl8586@naver.com>
2021-02-01 22:13:27 +0900

    Add roulette

--- src/containers/Home/_fragments/Roulette.js
+++ src/containers/Home/_fragments/Roulette.js
@@ -7,32 +7,52 @@ import Slider from "react-slick";
 const Roulette = ({ festivals }) => {
   const randomNumber = Math.floor(Math.random() * festivals.length);
   const [rouletteArray, setRouletteArray] = useState(festivals);
+  const [stop, setStop] = useState(true);
+
+  const startRoulette = () => {
+    setRouletteArray([...festivals, festivals[randomNumber]]);
+  };
 
   useEffect(() => {
-    setRouletteArray([...rouletteArray, rouletteArray[randomNumber]]);
+    startRoulette();
   }, []);
 
   const settings = {
+    initialSlide: -1,
     autoplay: true,
-    arrows: false,
     dots: false,
+    infinite: stop,
     slidesToShow: 1,
-    centerPadding: "10px",
-    draggable: false,
-    infinite: false,
-    pauseOnHover: false,
-    swipe: false,
-    touchMove: false,
+    slidesToScroll: 1,
     vertical: true,
     speed: 100,
     autoplaySpeed: 100,
+    afterChange: (slide) =>
+      slide === rouletteArray.length - 1 && setStop(false),
   };
 
   return (
     <RouletteContainer>
       <Text>오늘은</Text>
-      <Slider {...settings}></Slider>
+      {rouletteArray.length === festivals.length + 1 && (
+        <Slider {...settings}>
+          {rouletteArray.map((each, index) => (
+            <SliderContainer key={index}>
+              <Text>{each.title}</Text>
+            </SliderContainer>
+          ))}
+        </Slider>
+      )}
       <Text>어때요?</Text>
+      <Text
+        onClick={() => {
+          setRouletteArray(festivals);
+          startRoulette();
+          setStop(true);
+        }}
+      >
+        룰렛 돌리기
+      </Text>
     </RouletteContainer>
   );
 };
@@ -42,9 +62,19 @@ export default Roulette;
 const RouletteContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  margin: 20px;
+`;
+
+const SliderContainer = styled(Slider)`
+  .slick-slider {
+    height: 100px;
+  }
+  .slick-list {
+    overflow: hidden;
+  }
 `;
 
 const Text = styled.span`
   font-size: 23px;
+  text-align: center;
 `;
